04a2a5a0bfce95975934b64cf4db2c0b3c01b639
mac <mac@macs-MacBook-Air.local>
2022-07-02 11:52:14 +0100

    update

--- src/model/mysql/role.js
+++ src/model/mysql/role.js
@@ -1,25 +1,10 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RoleModel = void 0;
-/**
- * Create Role Model
- */
-class RoleModel {
-    /**
-     *
-     * @param sequelize pass new sequalize object
-     * @param Sequelize pass sequalize class
-     * @returns
-     */
-    constructor(sequelize, Sequelize) {
-        this._role = sequelize.define('role', {
-            name: {
-                type: Sequelize.STRING,
-                allowNull: false,
-                unique: true
-            }
-        });
-        return this._role;
-    }
-}
-exports.RoleModel = RoleModel;
+module.exports = (sequelize, Sequelize) => {
+    const Role = sequelize.define('role', {
+        name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true
+        }
+    });
+    return Role;
+};
